52d373fc45e9a98c47f813274a14c686c7cb0f3e
RevelsGit <revels0k@yandex.ru>
2017-12-06 14:07:19 +0200

    ...

--- package.js
+++ package.js
@@ -5,8 +5,7 @@ Package.describe({
   git: "https://github.com/softwarerero/meteor-accounts-t9n.git",
 });
 
-DEFAULT_LANGUAGES = ['ar', 'ca', 'cs', 'da', 'de', 'el', 'en', 'es', 
-  'es_ES', 'fa', 'fr', 'he', 'hr', 'hu', 'id', 'it', 'ja', 'kh', 'ko', 'nl', 'no_NB', 'pl', 'pt', 'pt_PT', 'ro', 'ru', 'sk', 'sl', 'sv', 'tr', 'uk', 'vi', 'zh_cn', 'zh_tw', 'zh_hk'];
+DEFAULT_LANGUAGES = ['en'];
 
 LANGUAGES = DEFAULT_LANGUAGES;
 if(process.env.T9N_LANGUAGES) {
